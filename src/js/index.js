a4f91da2ce2b72a0c14b34c2f5e0fd8f8776c7ec
Yasmim <ymdsc.coimbra85@gmail.com>
2025-10-04 14:24:59 -0300

    feat: implementar função que atualiza os dados recebidos e mostra na tela

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,20 @@ async function searchWeatherCondition(city) {
     const data = await response.json();
     return data;
 }
+
+function showDataOnScreen(data, city) {
+    const temperature = Math.round(data.current.temp_c);
+    const condition = data.current.condition.text;
+    const humidity = data.current.humidity;
+    const windSpeed = data.current.wind_kph;
+    const conditionIcon = data.current.condition.icon;
+
+    document.querySelector("#description").classList.add("hide");
+
+    document.querySelector("#city").textContent = city;
+    document.querySelector("#temperature").textContent = `${temperature} ºC`;
+    document.querySelector("#condition").textContent = condition;
+    document.querySelector("#humidity").textContent = `${humidity}%`;
+    document.querySelector("#wind-speed").textContent = `${windSpeed} km/h`;
+    document.querySelector("#condition-icon").setAttribute("src", conditionIcon);
+}
